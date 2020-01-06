239f2aeb91489bdb0c40cc5913209f937a15322e
Leandro Bleda Cantos <leandro.bleda-cantos@klarna.com>
2020-01-06 19:53:22 +0100

    count more verbose

--- count.sh
+++ count.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 topic=$1
-topic="./logs/${topic}"
-file=$(ls "${topic}")
-path="${topic}/${file}"
-tail -n 1 $path | cut -d : -f 3
+_topic="./logs/${topic}"
+file=$(ls "${_topic}")
+path="${_topic}/${file}"
+number=$(tail -n 1 ${path} | cut -d : -f 3)
+echo the number of consumed messages from the topic ${topic} is ${number}
